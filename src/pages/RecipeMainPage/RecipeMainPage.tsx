b2dbe2173fc211d2ba7b05c4c5d08ee79347a9a5
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-14 01:21:36 +0500

    added tailwindcss part to css file and renamed classnames with styles in main page

--- src/pages/RecipeMainPage/RecipeMainPage.tsx
+++ src/pages/RecipeMainPage/RecipeMainPage.tsx
@@ -1,7 +1,6 @@
 import { FC, useState, ChangeEvent } from 'react'
 // styles
-// import styles from "./index.module.scss";
-
+import styles from "./index.module.css";
 
 const RecipeAppMainPage: FC = () => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -16,31 +15,31 @@ const RecipeAppMainPage: FC = () => {
     };
 
     // Sample recipe data
-    // const recipes = [
-    //   { id: 1, name: 'Recipe 1', cookTime: 30 },
-    //   { id: 2, name: 'Recipe 2', cookTime: 45 },
-    //   { id: 3, name: 'Recipe 3', cookTime: 60 },
-    // ];
+    const recipes = [
+        { id: 1, name: 'Recipe 1', cookTime: 30 },
+        { id: 2, name: 'Recipe 2', cookTime: 45 },
+        { id: 3, name: 'Recipe 3', cookTime: 60 },
+    ];
 
     return (
-        <div className="container mx-auto px-4">
-            <header className="py-4">
-                <h1 className="text-2xl font-bold">Recipe App</h1>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h1 className={styles.textHeading}>Recipe App</h1>
             </header>
 
-            <div className="flex flex-wrap justify-between items-center mb-4">
-                <div className="w-full sm:w-auto mb-4 sm:mb-0">
+            <div className={`flex flex-wrap justify-between items-center mb-4`}>
+                <div className={`w-full sm:w-auto mb-4 sm:mb-0`}>
                     <input
                         type="text"
                         placeholder="Search..."
-                        className="border border-gray-400 rounded px-4 py-2 w-full sm:w-auto"
+                        className={styles.searchInput}
                         value={searchQuery}
                         onChange={handleSearchChange}
                     />
                 </div>
                 <div>
                     <select
-                        className="border border-gray-400 rounded px-4 py-2"
+                        className={styles.selectInput}
                         value={sortOption}
                         onChange={handleSortChange}
                     >
@@ -51,30 +50,25 @@ const RecipeAppMainPage: FC = () => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {[1, 2, 3, 4, 5, 6, 7].map(() => (
-                    <div
-                        //   key={recipe.id}
-                        className="border border-gray-400 rounded p-4"
-                    >
+            <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4`}>
+                {recipes.map((recipe) => (
+                    <div key={recipe.id} className={styles.recipeCard}>
                         <img
                             src="recipe-image.jpg"
-                            // alt={recipe.name}
-                            className="w-full h-48 object-cover mb-4"
+                            alt={recipe.name}
+                            className={styles.recipeImage}
                         />
-                        {/* <h2 className="text-xl font-bold mb-2">{recipe.name}</h2> */}
-                        <p className="text-gray-600 mt-2">
-                            {/* Cooking Time: {recipe.cookTime} minutes */}
+                        <h2 className={styles.recipeName}>{recipe.name}</h2>
+                        <p className={styles.recipeDetails}>
+                            Time: {recipe.cookTime} minutes
                         </p>
-                        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
-                            View Recipe
-                        </button>
+                        <button className={styles.viewRecipeButton}>View Recipe</button>
                     </div>
                 ))}
             </div>
 
-            <footer className="py-4 bg-white text-center">
-                <p className="text-gray-600">© 2023 Recipe App. All rights reserved.</p>
+            <footer className={styles.footer}>
+                <p className={styles.footerText}>&copy; 2023 Recipe App. All rights reserved.</p>
             </footer>
         </div>
     );
