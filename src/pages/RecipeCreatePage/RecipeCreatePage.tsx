0e03ee08e02727970666e8128877baebb89ea693
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-14 23:23:10 +0500

    added create page and connected backend link to test http work

---
+++ src/pages/RecipeCreatePage/RecipeCreatePage.tsx
@@ -0,0 +1,68 @@
+import { FC, useState, ChangeEvent, useEffect } from 'react';
+// styles
+import styles from './index.module.css';
+import axios from "axios";
+
+const RecipeCreatePage: FC = () => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    // const [image, setImage] = useState('');
+    const [cookTime, setCookTime] = useState('');
+    const [ingredients, setIngredients] = useState('');
+    const [recipes, setRecipes] = useState<any>([]);
+
+    useEffect(() => {
+        axios.get('http://backend.oliy-nemat.uz/recipe/').then((response) => {
+            setRecipes(response.data)
+        })
+    }, [])
+
+    console.log(recipes);
+
+    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // Send the form data to the backend API
+        // const formData = {
+        //     name,
+        //     description,
+        //     image,
+        //     cookTime,
+        //     ingredients: ingredients.split(',').map((ingredient) => ingredient.trim()),
+        // };
+
+        // Your API request logic here to send the formData to the backend
+        // Example: axios.post('/api/recipes', formData)
+    };
+
+    return (
+        <div className={styles.container}>
+            <h2>Create Recipe</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label className={styles.label}>Name:</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className={styles.input} />
+                </div>
+                <div>
+                    <label className={styles.label}>Description:</label>
+                    <textarea value={description} onChange={(e) => setDescription(e.target.value)} className={styles.input}></textarea>
+                </div>
+                <div>
+                    <label className={styles.label}>Image Upload:</label>
+                    <input type="file" className={styles.input} />
+                </div>
+                <div>
+                    <label className={styles.label}>Cook Time:</label>
+                    <input type="text" value={cookTime} onChange={(e) => setCookTime(e.target.value)} className={styles.input} />
+                </div>
+                <div>
+                    <label className={styles.label}>Ingredients (separated by comma):</label>
+                    <input type="text" value={ingredients} onChange={(e) => setIngredients(e.target.value)} className={styles.input} />
+                </div>
+                <button type="submit" className={styles.button}>Create Recipe</button>
+            </form>
+        </div>
+    );
+};
+
+export default RecipeCreatePage;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,16 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 // pages
 import RecipeMainPage from "../pages/RecipeMainPage/RecipeMainPage";
 import RecipeDetailsPage from "../pages/RecipeDetailsPage/RecipeDetailsPage";
+import RecipeCreatePage from "../pages/RecipeCreatePage/RecipeCreatePage";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
         element: <RecipeMainPage />
 
-    }, 
+    },
     {
         path: "/detail",
         element: <RecipeDetailsPage />
-
+    },
+    {
+        path: "/create",
+        element: <RecipeCreatePage />
     }
 ]) 
