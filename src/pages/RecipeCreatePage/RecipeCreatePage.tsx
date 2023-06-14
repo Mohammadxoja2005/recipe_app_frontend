751ac058b775bc84c4309271e0e62a002579ee7b
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-15 02:30:31 +0500

    separated create page into components

--- src/pages/RecipeCreatePage/RecipeCreatePage.tsx
+++ src/pages/RecipeCreatePage/RecipeCreatePage.tsx
@@ -1,56 +1,20 @@
-import { FC, useState, ChangeEvent } from 'react';
+import { FC } from 'react';
 // styles
 import styles from './index.module.css';
+// layouts
+import Header from '../../layouts/Header/Header';
+import Footer from '../../layouts/Footer/Footer';
+// components
+import RecipeForm from './components/RecipeForm/RecipeForm';
 
 const RecipeCreatePage: FC = () => {
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [image, setImage] = useState('');
-    const [cookTime, setCookTime] = useState('');
-    const [ingredients, setIngredients] = useState('');
-
-    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        // Send the form data to the backend API
-        // const formData = {
-        //     name,
-        //     description,
-        //     image,
-        //     cookTime,
-        //     ingredients: ingredients.split(',').map((ingredient) => ingredient.trim()),
-        // };
-
-        // Your API request logic here to send the formData to the backend
-        // Example: axios.post('/api/recipes', formData)
-    };
 
     return (
         <div className={styles.container}>
-            <h2>Create Recipe</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label className={styles.label}>Name:</label>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className={styles.input} />
-                </div>
-                <div>
-                    <label className={styles.label}>Description:</label>
-                    <textarea value={description} onChange={(e) => setDescription(e.target.value)} className={styles.input}></textarea>
-                </div>
-                <div>
-                    <label className={styles.label}>Image URL:</label>
-                    <input type="text" value={image} onChange={(e) => setImage(e.target.value)} className={styles.input} />
-                </div>
-                <div>
-                    <label className={styles.label}>Cook Time:</label>
-                    <input type="text" value={cookTime} onChange={(e) => setCookTime(e.target.value)} className={styles.input} />
-                </div>
-                <div>
-                    <label className={styles.label}>Ingredients (separated by comma):</label>
-                    <input type="text" value={ingredients} onChange={(e) => setIngredients(e.target.value)} className={styles.input} />
-                </div>
-                <button type="submit" className={styles.button}>Create Recipe</button>
-            </form>
+            <Header />
+            <RecipeForm />
+            <Footer />
+
         </div>
     );
 };
